fdbfebe43c29974c3df94d979d999b3c4f37d851
Joker <Joker@DESKTOP-GU3LRIJ.home>
2017-03-27 21:08:24 +0200

    Update Übungsblätter

--- Prog_Uebungsblaetter/src/Blatt1.java
+++ Prog_Uebungsblaetter/src/Blatt1.java
@@ -1,29 +1,82 @@
 
 /*
  * Aufgabe 1: Logische Ausdrücke
- * 
+ *
  * a) Geben Sie für jeden der folgenden Ausdrücke einen entsprechenden (möglichst kurzen) Java-Ausdruck an, der true
 liefert, wenn die Bedingung zutrifft, und ansonsten false. a, b, und c sind int-Variablen, d, e, f sind boolean-
 Variablen.
  */
-
 public class Blatt1 {
-	
-	 
-	 public static boolean No1A(){
-	 int a = 0;
-	 int b = 0;
-	 int c = 0;
-	 boolean d;
-	 boolean e;
-	 boolean f;
-	 
-	 	if(a == 0 && b ==0 && c ==0){
-	 		return true; 
-	 		}else{
-	 		return false;
-	 		}
-	 
-	 }
-}
+    protected static boolean No1A() {
+        int a = 0;
+        int b = 0;
+        int c = 0;
 
+        if ((a == 0) && (b == 0) && (c == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected static boolean No1B() {
+        int a = 0;
+
+        if (a <= 10 && a <=-10 && a%2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    protected static boolean No1C() {
+        int b = 95;
+
+        if (b%19 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    protected static boolean No1D() {
+        int c = 17;
+
+        if (c == 17 | c == 77 | c == 91 ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    protected static boolean No1E() {
+        boolean d = true;
+        boolean e = true;
+        boolean f = true;
+
+        if (d==true && e==true && f==true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    protected static boolean No1F() {  // wie alle Fälle einschließen?
+        boolean d = true;
+        boolean e = false;
+        boolean f = true;
+
+        if (d==true && e==false && f==true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+        	
+        
+    }
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
--- Prog_Uebungsblaetter/src/dieMain.java
+++ Prog_Uebungsblaetter/src/dieMain.java
@@ -3,7 +3,12 @@
 public class dieMain extends Blatt1 {
 	 public static void main(String args[]) {
 		 
-         System.out.println(Blatt1.No1A());
+         System.out.println("Nummer 1a ist" + " " + Blatt1.No1A()+".");
+         System.out.println("Nummer 1b ist" + " " + Blatt1.No1B()+".");
+         System.out.println("Nummer 1c ist" + " " + Blatt1.No1C()+".");
+         System.out.println("Nummer 1d ist" + " " + Blatt1.No1D()+".");
+         System.out.println("Nummer 1e ist" + " " + Blatt1.No1E()+".");
+         System.out.println("Nummer 1f ist" + " " + Blatt1.No1F()+".");
 		 
 
 	 }
