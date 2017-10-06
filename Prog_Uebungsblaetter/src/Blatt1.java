89b6a4b60a9a15b9ef7abc7c33d666904b098782
student <student@Linux>
2017-10-06 18:55:43 +0200

    dict

--- Prog_Uebungsblaetter/src/Blatt1.java
+++ Prog_Uebungsblaetter/src/Blatt1.java
@@ -1,12 +1,12 @@
 public class Blatt1 {
 
     /*
-     *  > Aufgabe 2: Die Winkelmaßeinheit Gon <
-     * Das Gon ist eine Hilfsmaßeinheit zur Angabe derWinkelweite ebenerWinkel, die sich letztendlich nur im Vermessungswesen-
-     * wesen etablieren kontte. Als Einheitszeichen wird "gon" verwendet. Ein Gon ist definiert als der vierhundertste Teil
-     * des Vollwinkels, dh.h, 1 Vollwinkel = 400g = 360°.
+     *  > Aufgabe 2: Die WinkelmaÃŸeinheit Gon <
+     * Das Gon ist eine HilfsmaÃŸeinheit zur Angabe derWinkelweite ebenerWinkel, die sich letztendlich nur im Vermessungswesen
+     * etablieren konnte. Als Einheitszeichen wird "Gon" verwendet. Ein Gon ist definiert als der vierhundertste Teil
+     * des Vollwinkels, dh.h, 1 Vollwinkel = 400g = 360ï¿½.
      *
-     * Schreiben Sie ein Java-Programm, dass eine beliebige ganze Zahl in der EInheit Gon korrekt in das Gradmaß überführt
+     * Schreiben Sie ein Java-Programm, dass eine beliebige ganze Zahl in der EInheit Gon korrekt in das GradmaÃŸ Ã¼berfÃ¼hrt
      * und ausgibt. Beachten Sie, dass das Ergebnis eine Gleitkommazahl sein kann.
      */
     protected static String Aufgabe2() {
@@ -17,15 +17,15 @@ public class Blatt1 {
         faktor   = 360.0 / 400.0;
         ergebnis = i * faktor;
 
-        String f = ("B1.Aufgabe2: "+i+ " " + "Gon sind umgerechnet " + ergebnis + "°");
+        String f = ("B1.Aufgabe2: "+i+ " " + "Gon sind umgerechnet " + ergebnis + "ï¿½");
 
         return f;
     }
 
     /*
      *  > Aufgabe 3: Ballistische Kurve <
-     * Schreiben Sie ein Programm Balistic, das mithilfe der Formel x0+v0*t-(g*t^2)/2 das Ergebnis für drei beliebigen
-     * positiven Fließkommazahlen x0, v0 und t berechnet. Die Formel berechnet die Position eines Körpers nach der Zeit t,
+     * Schreiben Sie ein Programm Balistic, das mithilfe der Formel x0+v0*t-(g*t^2)/2 das Ergebnis fÃ¼r drei beliebigen
+     * positiven FlieÃŸkommazahlen x0, v0 und t berechnet. Die Formel berechnet die Position eines KÃ¶rpers nach der Zeit t,
      * der von Position x0 aus mit einer Geschwindigkeit v0 nach oben geworfen wird. Die Konstante g = 9; 81274 m/s^2 ist
      * die Erdbeschleunigung.
      */
@@ -34,21 +34,21 @@ public class Blatt1 {
         double geschwindigkeit   = 5.0;        // v
         double zeit              = 6.0;
         double erdbeschleunigung = 9.81274;    // g
-        double endposition;                    // formel inkludiert
+        double endposition;                    // Formel inkludiert
 
         endposition = ((anfangsposition + geschwindigkeit) * zeit) - (erdbeschleunigung * (Math.pow(zeit, 2))) / 2;
 
-        String f = ("B1.Aufgabe3: "+"Der Körper auf Position " + anfangsposition + " befindet sich nach " + zeit + " Sekunden"
+        String f = ("B1.Aufgabe3: "+"Der KÃ¶rper auf Position " + anfangsposition + " befindet sich nach " + zeit + " Sekunden"
                     + "an der Endposition " + endposition);
 
-        return f;    // Ausgabe ist -116.62932, korrekt wäre -146.62932
+        return f;    // Ausgabe ist -116.62932, korrekt wï¿½re -146.62932
     }
 
     /*
      *  > Aufgabe 4: Zufallszahlen <
      * Schreiben Sie ein Java-Programm, welches eine Zufallszahl zwischen denWerten der beiden Variablen n und m erzeugt.
-     * Die Zufallszahl soll dabei auch die Werte von n und m annehmen können. Die Erzeugung von Zufallszahlen zwischen n
-     * und m kann beispielsweise zur Simulation von Glücksspielen (Lotto 6 aus 49, etc.) verwendet werden.
+     * Die Zufallszahl soll dabei auch die Werte von n und m annehmen kÃ¶nnen. Die Erzeugung von Zufallszahlen zwischen n
+     * und m kann beispielsweise zur Simulation von GlÃ¼cksspielen (Lotto 6 aus 49, etc.) verwendet werden.
      */
     protected static void Aufgabe4() {
         int m           = 6;
@@ -56,7 +56,7 @@ public class Blatt1 {
         int zufallszahl = (int) (Math.random() * 44) + 6;
         
         System.out.println("B1.Aufgabe4: "+"Zufallszahl: "+zufallszahl);
-        //return zufallszahl;
+        //return Zufallszahl;
     }
 
     /*
@@ -90,8 +90,8 @@ public class Blatt1 {
     }
 
     /*
-     *  > Aufgabe 1: Logische Ausdrücke <
-     * Geben Sie für jeden der folgenden Ausdrücke einen entsprechenden (möglichst kurzen) Java-Ausdruck an, der true
+     *  > Aufgabe 1: Logische AusdrÃ¼cke <
+     * Geben Sie fï¿½r jeden der folgenden AusdrÃ¼cke einen entsprechenden (mï¿½glichst kurzen) Java-Ausdruck an, der true
      * liefert, wenn die Bedingung zutrifft, und ansonsten false. a, b, und c sind int-Variablen, d, e, f sind boolean-
      * Variablen.
      */
