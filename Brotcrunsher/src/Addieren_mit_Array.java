7e096872f7e21a2748db5fdb1f2daf95993d62b0
g.uru <22568014+g-uru@users.noreply.github.com>
2018-02-05 15:59:28 +0100

    test

--- Brotcrunsher/src/Addieren_mit_Array.java
+++ Brotcrunsher/src/Addieren_mit_Array.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Addieren_mit_Array {
 	public static void main(String args[]){
 		Scanner s = new Scanner(System.in);
-		
+
 
 		int[] i = new int[3];
 		System.out.println("Nenne zwei Zahlen zum Addieren");
@@ -12,12 +12,12 @@ public class Addieren_mit_Array {
 		i[1]=s.nextInt();
 
 		i[2]=i[0]+i[1];
-		
-		
+
+
 		System.out.println(i[0] +" + "+i[1]+" = "+ i[2]);
-		
 
 
-		
+
+
 	}
 }
