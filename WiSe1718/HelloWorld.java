824b491b1b1e4b94719b0e441234e1305c5df3fb
g-uru <22568014+g-uru@users.noreply.github.com>
2018-02-24 18:29:11 +0100

    commit

--- WiSe1718/HelloWorld.java
+++ WiSe1718/HelloWorld.java
@@ -1,9 +1,17 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        test
+    public static void main(String[] args) { 
+        int year = Integer.parseInt(args[0]);
+        boolean isLeapYear;
+
+        // divisible by 4
+        isLeapYear = (year % 4 == 0);
+
+        // divisible by 4 and not 100
+        isLeapYear = isLeapYear && (year % 100 != 0);
+
+        // divisible by 4 and not 100 unless divisible by 400
+        isLeapYear = isLeapYear || (year % 400 == 0);
+
+        System.out.println(isLeapYear);
     }
 }
-
-
-//~ Formatted by Jindent --- http://www.jindent.com
