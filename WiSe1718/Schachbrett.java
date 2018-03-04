01c5cf104a51f9c90702b18561f296eaa531b2ca
g-uru <22568014+g-uru@users.noreply.github.com>
2018-03-04 18:57:07 +0100

    2018

--- WiSe1718/Schachbrett.java
+++ WiSe1718/Schachbrett.java
@@ -1,8 +1,24 @@
+/******************************************************************************
+ *  Compilation:  javac Schachbrett.java
+ *  Execution:    java Schachbrett n
+ *  Test:         ./gradlew testen
+ *
+ *  Eingabe: Eine positive, ganze Zahl n.
+ *  Ausgabe: Ein Schachbrett der Groesse n * n. Schwarze Felder werden durch einen
+ *  Stern '*', weisse Felder durch ein Leerzeichen ' ' dargestellt. Das Brett
+ *  beginnt mit einem schwarzen Feld.
+ *
+ * Ueberpruefen Sie, dass die Eingabe positiv ist. Wenn die Eingabe nicht positiv ist,
+ * soll das Programm ERROR ausgeben. Andere Fehleingaben koennen Sie ignorieren.
+ * 
+ ******************************************************************************/
+
 import java.util.Scanner;
 
 public class Schachbrett {
 
 	public static void main(String[] args) {
+		
 		/*
 		 * int n = Integer.parseInt(args[0]); Scanner s = new Scanner(System.in);
 		 * System.out.println("Zahl eingeben"); int n = s.nextInt(); if (n < 1) {
@@ -13,6 +29,7 @@ public class Schachbrett {
 		 * }
 		 */
 		//int n = Integer.parseInt(args[0]);
+		
 		Scanner s = new Scanner(System.in);
 		System.out.println("Zahl eingeben");
 		int n = s.nextInt();
