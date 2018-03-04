08a948b66267cf8f8129a5e508c7572db404c2a7
g-uru <22568014+g-uru@users.noreply.github.com>
2018-03-04 19:09:20 +0100

    2018

--- WiSe1718/Schachbrett.java
+++ WiSe1718/Schachbrett.java
@@ -20,41 +20,23 @@ public class Schachbrett {
 
 	public static void main(String[] args) {
 
-		/*
-		int n = Integer.parseInt(args[0]);
+		// int n = Integer.parseInt(args[0]);
+
 		Scanner s = new Scanner(System.in);
 		System.out.println("Zahl eingeben");
 		int n = s.nextInt();
-		if (n < 1) {
+		if (n < 1)
 			System.out.println("ERROR");
-		} else {
-			int tmp = n;
-			for (int i = 0; i <= tmp; i++) {
-				if (i % 2 == 0) {
-					System.out.println("*");
-				} else if (!(i % 2 == 0))
+		return;
+
+		int tmp = n;
+		for (int i = 0; i <= tmp; i++) {
+			if (i % 2 == 0) {
+				System.out.println("*");
+				if (!(i % 2 == 0))
 					;
-				{
-
-				}
-		*/
-
-				// int n = Integer.parseInt(args[0]);
-
-				Scanner s = new Scanner(System.in);
-				System.out.println("Zahl eingeben");
-				int n = s.nextInt();
-
-				for (int i = 1; i <= n; i++) {
-					for (int j = i; j <= n + i - 1; j++)
-						if (j < 10 && j != i)
-							System.out.print(" " + "*" + " ");
-						else
-							System.out.print("*" + " ");
-					System.out.println();
-				}
 			}
-
 		}
-	/*}
-}*/
+
+	}
+}
