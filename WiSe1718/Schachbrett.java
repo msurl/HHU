59a46561231886bc473a0f5a30e50ae6ef016514
g-uru <22568014+g-uru@users.noreply.github.com>
2018-03-04 19:01:42 +0100

    2018

--- WiSe1718/Schachbrett.java
+++ WiSe1718/Schachbrett.java
@@ -1,3 +1,4 @@
+
 /******************************************************************************
  *  Compilation:  javac Schachbrett.java
  *  Execution:    java Schachbrett n
@@ -18,31 +19,42 @@ import java.util.Scanner;
 public class Schachbrett {
 
 	public static void main(String[] args) {
-		
+
 		/*
-		 * int n = Integer.parseInt(args[0]); Scanner s = new Scanner(System.in);
-		 * System.out.println("Zahl eingeben"); int n = s.nextInt(); if (n < 1) {
-		 * System.out.println("ERROR"); } else { int tmp = n; for (int i = 0; i <= tmp;
-		 * i++) { if(i%2==0) { System.out.println("*");} else if (!(i%2==0));{
-		 * System.out.print(" ");} }
-		 * 
-		 * }
-		 */
-		//int n = Integer.parseInt(args[0]);
-		
+		int n = Integer.parseInt(args[0]);
 		Scanner s = new Scanner(System.in);
 		System.out.println("Zahl eingeben");
 		int n = s.nextInt();
-		
+		if (n < 1) {
+			System.out.println("ERROR");
+		} else {
+			int tmp = n;
+			for (int i = 0; i <= tmp; i++) {
+				if (i % 2 == 0) {
+					System.out.println("*");
+				} else if (!(i % 2 == 0))
+					;
+				{
 
-		for (int i = 1; i <= n; i++) {
-			for (int j = i; j <= n + i - 1; j++)
-				if (j < 10 && j != i)
-					System.out.print(" " + "*" + " ");
-				else
-					System.out.print("*" + " ");
-			System.out.println();
+				}
+		*/
+
+				// int n = Integer.parseInt(args[0]);
+
+				Scanner s = new Scanner(System.in);
+				System.out.println("Zahl eingeben");
+				int n = s.nextInt();
+
+				for (int i = 1; i <= n; i++) {
+					for (int j = i; j <= n + i - 1; j++)
+						if (j < 10 && j != i)
+							System.out.print(" " + "*" + " ");
+						else
+							System.out.print("*" + " ");
+					System.out.println();
+				}
+			}
+
 		}
-	}
-
-}
+	/*}
+}*/
